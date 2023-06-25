014c11a56d6467a17ee5457a147bb9ce2ff7cd77
martinrotela <martinrotela47@gmail.com>
2023-06-25 06:42:01 -0300

    redux set up

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
+import { authSlice } from './features/authSlice';
+
+export const store = configureStore({
+    reducer: {
+        [authSlice.name]: authSlice.reducer,
+    },
+    devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
